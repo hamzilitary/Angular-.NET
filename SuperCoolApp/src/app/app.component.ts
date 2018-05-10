fd28c142af781091fab1b88a50d84b1a0e8bf71c
Joel Adams <joeladamsdesign@gmail.com>
2018-05-10 10:38:45 -0700

    add homepage component

--- SuperCoolApp/src/app/app.component.ts
+++ SuperCoolApp/src/app/app.component.ts
@@ -1,16 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
+import { ToDo } from './models/to-do.model';
+import { TaskService } from './services/task.service'
+import { Router } from '@angular/router';
+
+
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-    constructor(private _httpService: Http) { }
-    apiValues: string[] = [];
-    ngOnInit() {
-        this._httpService.get('/api/values').subscribe(values => {
-            this.apiValues = values.json() as string[];
-        });
-    }
+
+
+export class AppComponent {
+  title = 'Weather Or Not';
+  constructor() {
+    //tasks: any;
+
+
+    //filterBySuggest: string = "All Tasks";
+    //filtered: boolean = false;
+    //constructor(private taskService: TaskService, private router: Router) { }
+
+    ////get filteredTasks() {
+    ////  if (!this.filtered) {
+    ////    return this.tasks;
+    ////  }
+    ////  let filterFunc = function (task) {
+    ////    return (task.suggest && this.filterBySuggest === 'Suggested') || !task.suggest
+    ////  };
+    ////  return this.tasks.flatMap(t => t).filter<any>(filterFunc);
+    ////}
+    //ngOnInit() {
+    //  this.tasks = this.taskService.getTasks();
+    //}
+
+    //goToDetail(clicked) {
+    //  this.router.navigate(['tasks', clicked.$key]);
+    //}
+
+
+
+    //setFilterCategory(suggest: string) {
+    //  this.filterBySuggest = suggest;
+    //  this.filtered = true;
+    //}
+
+    //resetFilterCategory() {
+    //  this.filterBySuggest = 'All Tasks'
+    //  this.filtered = false;
+    //}
+
+
+
+  }
 }
--- SuperCoolApp/src/app/homepage/homepage.component.ts
+++ SuperCoolApp/src/app/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 import { ToDo } from '../models/to-do.model';
 import { HttpClient } from '@angular/common/http';
 
@@ -12,41 +12,41 @@ import { TaskService } from '../services/task.service'
   providers: []
 })
 
-export class HomepageComponent implements OnInit {
-  tasks: any;
+export class HomepageComponent   {
+  //tasks: any;
   
-  filterBySuggest: string = "All Tasks";
-  filtered: boolean = false;
-  constructor(private taskService: TaskService, private router: Router) { }
+  //filterBySuggest: string = "All Tasks";
+  //filtered: boolean = false;
+  //constructor(private taskService: TaskService, private router: Router) { }
 
-  //get filteredTasks() {
-  //  if (!this.filtered) {
-  //    return this.tasks;
-  //  }
-  //  let filterFunc = function (task) {
-  //    return (task.suggest && this.filterBySuggest === 'Suggested') || !task.suggest
-  //  };
-  //  return this.tasks.flatMap(t => t).filter<any>(filterFunc);
+  ////get filteredTasks() {
+  ////  if (!this.filtered) {
+  ////    return this.tasks;
+  ////  }
+  ////  let filterFunc = function (task) {
+  ////    return (task.suggest && this.filterBySuggest === 'Suggested') || !task.suggest
+  ////  };
+  ////  return this.tasks.flatMap(t => t).filter<any>(filterFunc);
+  ////}
+  //ngOnInit() {
+  //  this.tasks = this.taskService.getTasks();
   //}
-  ngOnInit() {
-    this.tasks = this.taskService.getTasks();
-  }
 
-  goToDetail(clicked) {
-    this.router.navigate(['tasks', clicked.$key]);
-  }
+  //goToDetail(clicked) {
+  //  this.router.navigate(['tasks', clicked.$key]);
+  //}
 
 
 
-  setFilterCategory(suggest: string) {
-    this.filterBySuggest = suggest;
-    this.filtered = true;
-  }
+  //setFilterCategory(suggest: string) {
+  //  this.filterBySuggest = suggest;
+  //  this.filtered = true;
+  //}
 
-  resetFilterCategory() {
-    this.filterBySuggest = 'All Tasks'
-    this.filtered = false;
-  }
+  //resetFilterCategory() {
+  //  this.filterBySuggest = 'All Tasks'
+  //  this.filtered = false;
+  //}
 
 
 
